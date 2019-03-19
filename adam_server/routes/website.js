f7d9f7dec2985b4fabbc934bdc3f9566fe7217fd
sa2423 <seerat.aziz15@gmail.com>
2019-03-19 19:57:22 -0400

    cleaned up UI's for menu/recommendation/reservation pages. uploaded reports

--- adam_server/routes/website.js
+++ adam_server/routes/website.js
@@ -7,18 +7,42 @@ router.get('/', function(req, res, next) {
 	res.render('index');
 });
 
+/* MENU AND SUBPAGES*/
 router.get('/menu', function(req, res, next){
 	res.render('menu'); 
 });
 
+router.get('/popular-dishes', function(req, res, next){
+	res.render('popular_dishes');
+});
+
+router.get('/appetizers', function(req, res, next){
+	res.render('appetizers');
+});
+
+router.get('/entrees', function(req, res, next){
+	res.render('entrees');
+});
+
+router.get('/desserts', function(req, res, next){
+	res.render('desserts');
+});
+
+router.get('/drinks', function(req, res, next){
+	res.render('drinks');
+});
+
+/* RECOMMEND ME FEATURE */
 router.get('/recommend-me', function(req, res, next) {
 	res.render('recommend');
 });
 
+/* CART */
 router.get('/cart', function(req, res, next) {
 	res.render('cart'); 
 });
 
+/* RESERVE TABLE */
 router.get('/reserve', function(req, res, next) {
 	res.render('reserve');
 });
